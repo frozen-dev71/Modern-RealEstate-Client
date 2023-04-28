4657dd97af854c516962113f15d0901c417a79ea
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-04-28 22:45:50 +0300

    Adds a Animations file to maintain which animation play

---
+++ src/components/isolated/animations/Animations.jsx
@@ -0,0 +1,32 @@
+import { useLayoutEffect } from 'react';
+import { AnimatePresence } from 'framer-motion';
+import Entrance from './entrance/Entrance';
+import TransitionMount from './transitionMount/TransitionMount';
+import TransitionUnmount from './transitionUnmount/TransitionUnmount';
+import { useEntranceContext } from '../../../context/entranceContext';
+import { removeScrollbar, addScrollbar } from '../../../utils/utilities';
+
+const Animations = () => {
+  const { isEntranceActive } = useEntranceContext();
+
+  // Remove scroll if entrance is active
+  useLayoutEffect(() => {
+    if (!isEntranceActive) return;
+
+    // remove scroll
+    removeScrollbar();
+  }, [isEntranceActive]);
+
+  return (
+    <>
+      <AnimatePresence onExitComplete={addScrollbar}>
+        {isEntranceActive ? <Entrance /> : null}
+      </AnimatePresence>
+
+      <TransitionMount isEntranceActive={isEntranceActive} />
+      <TransitionUnmount />
+    </>
+  );
+};
+
+export default Animations;
