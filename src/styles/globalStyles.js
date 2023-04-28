127fbe7da49db7448a75cf73e22b5dd91ae4a83c
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-04-28 23:23:01 +0300

    Adds a file for global styling

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+import { reset, variables, barlowFonts, utilityClasses, thirdPartyStyles } from './helpers';
+
+const GlobalStyles = createGlobalStyle`
+${reset}
+${variables};
+${barlowFonts}; 
+${utilityClasses}
+${thirdPartyStyles}
+`;
+
+export default GlobalStyles;
