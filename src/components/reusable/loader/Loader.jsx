fa4165b204eaeb5f9c6bd3fabb3eb4afbcf0d61b
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-04-28 23:15:51 +0300

    Adds Loader reusable component

---
+++ src/components/reusable/loader/Loader.jsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import { respondTo } from '../../../styles/helpers';
+
+const Loader = () => {
+  return (
+    <StyledLoader>
+      <span className='loader' />
+    </StyledLoader>
+  );
+};
+
+// Styles
+const StyledLoader = styled.div`
+  .loader {
+    width: 3.5rem;
+    height: 3.5rem;
+    border: 5px solid var(--clr-dark);
+    border-bottom-color: transparent;
+    border-radius: 50%;
+    display: inline-block;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+  }
+
+  @keyframes rotation {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  ${respondTo.xl`
+    .loader {
+        width: 5rem;
+        height: 5rem;
+    }
+  `}
+`;
+
+export default Loader;
