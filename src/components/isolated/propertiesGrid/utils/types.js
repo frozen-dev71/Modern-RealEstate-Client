c9ec6da0419468d678e8654f832e49dd61461b35
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-04-29 17:14:05 +0300

    Adds utilities files for grid properties

---
+++ src/components/isolated/propertiesGrid/utils/types.js
@@ -0,0 +1,10 @@
+export const UPDATE_FILTER = 'UPDATE_FILTER';
+export const FILL_PROPERTIES_ARRAY = 'FILL_PROPERTIES_ARRAY';
+export const FILTER_PROPERTIES_ARRAY = 'FILTER_PROPERTIES_ARRAY';
+
+export const filters = [
+  { content: 'All', type: 'all' },
+  { content: 'On sale', type: 'on sale' },
+  { content: 'Soon', type: 'coming soon' },
+  { content: 'Sold', type: 'sold' },
+];
